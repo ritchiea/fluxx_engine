65e84694ea0cb7fcb25d9b577f329fedac3deaa0
Eric Hansen <ehansen@eric-hansens-macbook-pro.local>
2010-06-17 10:46:54 -0700

    add musicians to the dummy schema

--- test/dummy/db/migrate/20100616232433_create_musicians.rb
+++ test/dummy/db/migrate/20100616232433_create_musicians.rb
@@ -1,8 +1,15 @@
 class CreateMusicians < ActiveRecord::Migration
   def self.up
     create_table :musicians do |t|
-
       t.timestamps
+      t.string :first_name
+      t.string :last_name
+      t.string :music_type
+      t.string :street_address
+      t.string :city
+      t.string :state
+      t.string :zip
+      t.date :date_of_birth
     end
   end
 
