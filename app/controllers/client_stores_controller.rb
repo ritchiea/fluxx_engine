8da92b08063b329c7fe257835ff044a2d924e26f
Eric Hansen <ehansen@eric-hansens-macbook-pro.local>
2010-08-29 10:31:55 -0700

    add current user to client store

--- app/controllers/client_stores_controller.rb
+++ app/controllers/client_stores_controller.rb
@@ -21,6 +21,10 @@ class ClientStoresController < ApplicationController
     end
   end
   insta_post ClientStore do |insta|
+    insta.pre do |conf, controller|
+      controller.pre_model = ClientStore.new controller.params[:client_store]
+      controller.pre_model.user_id = controller.fluxx_current_user.id if controller.fluxx_current_user
+    end
     insta.format do |format|
       format.json do |controller_dsl, controller, outcome|
         controller.render :inline => outcome.to_s
