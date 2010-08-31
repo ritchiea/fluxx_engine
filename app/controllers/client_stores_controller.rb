5bfceeacced929725ff293d192045e482eb4cf16
Eric Hansen <ehansen@eric-hansens-macbook-pro.local>
2010-08-31 09:53:42 -0700

    same API should work with regular insta methods

--- app/controllers/client_stores_controller.rb
+++ app/controllers/client_stores_controller.rb
@@ -1,17 +1,5 @@
 class ClientStoresController < ApplicationController
   insta_index ClientStore do |insta|
-    insta.pre do |controller_dsl, controller|
-      controller.pre_models = if controller.fluxx_current_user
-        search = ClientStore.where(:user_id => controller.fluxx_current_user.id).where(:deleted_at => nil)
-        if controller.params[:name]
-          search = search.where(:name => controller.params[:name]) 
-        end
-        results = search.all
-      else
-        []
-      end
-    end
-    
     insta.format do |format|
       format.json do |controller_dsl, controller, outcome|
         controller.render :inline => controller.instance_variable_get("@models").to_json
@@ -24,10 +12,8 @@ class ClientStoresController < ApplicationController
       controller.pre_model.user_id = controller.fluxx_current_user.id if controller.fluxx_current_user
     end
   end
-  insta_put ClientStore do |insta|
-  end
-  insta_delete ClientStore do |insta|
-  end
+  insta_put ClientStore
+  insta_delete ClientStore
   insta_show ClientStore do |insta|
     insta.template = 'client_store_show'
   end
