bf6635e4610d06d294d1f97bfd962a4da4caf415
Eric Hansen <ehansen@eric-hansens-macbook-pro.local>
2010-09-03 10:47:25 -0700

    revert back to use custom pre method to load client_stores; need to limit by current_user

--- app/controllers/client_stores_controller.rb
+++ app/controllers/client_stores_controller.rb
@@ -1,5 +1,19 @@
 class ClientStoresController < ApplicationController
   insta_index ClientStore do |insta|
+    insta_index ClientStore do |insta|
+      insta.pre do |controller_dsl, controller|
+        controller.pre_models = if controller.fluxx_current_user
+          search = ClientStore.where(:user_id => controller.fluxx_current_user.id).where(:deleted_at => nil)
+          if controller.params[:name]
+            search = search.where(:name => controller.params[:name]) 
+          end
+          results = search.all
+        else
+          []
+        end
+      end
+    end
+    
     insta.format do |format|
       format.json do |controller_dsl, controller, outcome|
         controller.render :inline => controller.instance_variable_get("@models").map {|model| {:client_store => (model.attributes), :url => controller.url_for(model)}}.to_json
