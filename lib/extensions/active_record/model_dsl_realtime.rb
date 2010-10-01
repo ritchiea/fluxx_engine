a797d5af931e4b3a56147aadc95cde7178d4b8a6
Eric Hansen <ehansen@eric-hansens-macbook-pro.local>
2010-10-01 16:30:57 -0700

    make a facility to twiddle the model_class based on the whim of the controller

--- lib/extensions/active_record/model_dsl_realtime.rb
+++ lib/extensions/active_record/model_dsl_realtime.rb
@@ -25,20 +25,28 @@ class ActiveRecord::ModelDslRealtime < ActiveRecord::ModelDsl
     end
   end
   
+  def realtime_class_name model
+    if model.respond_to? :realtime_classname
+      model.realtime_classname
+    else
+      model.class.name
+    end
+  end
+  
   def realtime_create_callback model
-    write_realtime(model, :action => 'create', :user_id => realtime_user_id(model), :model_id => realtime_model_id(model), :model_class => model.class.name, :type_name => model.class.name, :delta_attributes => calculate_attributes(model).to_json)
+    write_realtime(model, :action => 'create', :user_id => realtime_user_id(model), :model_id => realtime_model_id(model), :model_class => realtime_class_name(model), :type_name => model.class.name, :delta_attributes => calculate_attributes(model).to_json)
   end
   
   def realtime_update_callback model
     if model.respond_to?(:deleted_at) && !model.deleted_at.blank?
       realtime_destroy_callback model
     else
-      write_realtime(model, :action => 'update', :user_id => realtime_user_id(model), :model_id => realtime_model_id(model), :model_class => model.class.name, :type_name => model.class.name, :delta_attributes => calculate_attributes(model).to_json)
+      write_realtime(model, :action => 'update', :user_id => realtime_user_id(model), :model_id => realtime_model_id(model), :model_class => realtime_class_name(model), :type_name => model.class.name, :delta_attributes => calculate_attributes(model).to_json)
     end
   end
   
   def realtime_destroy_callback model
-    write_realtime(model, :action => 'delete', :user_id => realtime_user_id(model), :model_id => realtime_model_id(model), :model_class => model.class.name, :type_name => model.class.name, :delta_attributes => calculate_attributes(model).to_json)
+    write_realtime(model, :action => 'delete', :user_id => realtime_user_id(model), :model_id => realtime_model_id(model), :model_class => realtime_class_name(model), :type_name => model.class.name, :delta_attributes => calculate_attributes(model).to_json)
   end
   
   def write_realtime  model, params
