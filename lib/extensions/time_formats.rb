bef2d7f55f0ee908d3eca8eb2447e80650e990d9
Eric Hansen <ehansen@eric-hansens-macbook-pro.local>
2010-10-12 16:44:48 -0700

    FLX-23 adjust action buttons on request detail view.  Move the template to include the whole footer

--- lib/extensions/time_formats.rb
+++ lib/extensions/time_formats.rb
@@ -11,6 +11,9 @@ Time::DATE_FORMATS[:hours_minutes_ampm] = "%I:%M %p"
 Time::DATE_FORMATS[:star_hours_minutes_ampm] = "*%I:%M %p"
 Time::DATE_FORMATS[:msoft] = "%Y-%m-%dT%H:%M:%S.000"
 Time::DATE_FORMATS[:sql] = "%Y-%m-%d"
+Time::DATE_FORMATS[:hgrant] = "%Y%m%dT%H:%M-0000"
+Time::DATE_FORMATS[:month_year] = "%B, %Y"
+Time::DATE_FORMATS[:abbrev_month_year] = "%b, %Y"
 
 
 def strip_zeros_from_date(marked_date_string)
@@ -18,8 +21,18 @@ def strip_zeros_from_date(marked_date_string)
 end
 
 class Time
+  def abbrev_month_year
+    self.to_s(:abbrev_month_year)
+  end
+  def month_year
+    self.to_s(:month_year)
+  end
   def msoft
     self.to_s(:msoft)
+  end
+  
+  def hgrant
+    self.to_s(:hgrant)
   end
   
   def sql
