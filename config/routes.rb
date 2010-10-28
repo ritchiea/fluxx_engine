e12a4e504b6df83c9799314086550332d755902d
Eric Hansen <ehansen@eric-hansens-macbook-pro.local>
2010-10-28 09:28:11 -0700

    FLX-117 moving dashboard controller and routing to fluxx_engine plus a default view that should be overridden

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   resources :realtime_updates
   resources :client_stores
+  match 'dashboard', :to => 'dashboard#index'
+  get "dashboard/index"
+  get "dashboard/example_partial_update"
+  root :to => 'dashboard#index'
+  resources :user_sessions
 end
