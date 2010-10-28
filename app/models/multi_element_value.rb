cee3384099cabff4740c381d6d081477cabad944
Eric Hansen <ehansen@eric-hansens-macbook-pro.local>
2010-10-28 13:19:38 -0700

    checkin change to support dependent multi value elements

--- app/models/multi_element_value.rb
+++ app/models/multi_element_value.rb
@@ -1,6 +1,7 @@
 class MultiElementValue < ActiveRecord::Base
   has_many :multi_element_choices
   belongs_to :multi_element_group
+  belongs_to :dependent_multi_element_value, :class_name => 'MultiElementValue', :foreign_key => 'dependent_multi_element_value_id'
   
   def to_s
     description || value
