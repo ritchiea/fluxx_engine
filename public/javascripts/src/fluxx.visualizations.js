64e15586d9fcbb564b3918df186b0d6564ec70ac
Andrew Lee <drew@Andrew-Lees-MacBook-Pro.local>
2011-01-09 11:06:11 -1000

    FLX-49 Visualizations

--- public/javascripts/src/fluxx.visualizations.js
+++ public/javascripts/src/fluxx.visualizations.js
@@ -1,35 +1,36 @@
 (function($){
   $.fn.extend({
     renderChart: function() {
-      var dataURL = '';
       return this.each(function() {
+
         var $chart = $(this);
+        if ($chart.children().length > 0)
+          return;
+
+        var $card = $chart.fluxxCard();
+
+        var data = $.parseJSON($chart.html());
+//        $chart.show();
+//        return;
+        $chart.html('').show().parent();
         var chartID = 'chart' + $.fluxx.visualizations.counter++;
 
-        var dataURL = $chart.attr('data-src');
-        var query = {'request_ids': $chart.attr('request-ids')};
-        if (dataURL) {
-          //TODO: This isn't working
-          //$('.loading-indicator', $chart.fluxxCard()).addClass('loading');
+        if (data) {
+          $chart.html("").append('<div id="' + chartID + '"></div>');
+          $.jqplot.config.enablePlugins = true;
 
-          $.getJSON(dataURL, query, function(data, status) {
-            $chart.html("").append('<div id="' + chartID + '"></div>');
-            //$('.loading-indicator', $chart.fluxxCard()).removeClass('loading');
-            $.jqplot.config.enablePlugins = true;
-
-            plot1 = $.jqplot(chartID, data.data, {
-              title: data.title,
-              height: 450,
-              width: 220,
-              stackSeries: true,
-              legend:{show: true},
-              grid:{background:'#fefbf3', borderWidth:2.5},
-              seriesDefaults: data.seriesDefaults,
-              axes: data.axes,
-              series: data.series
-            });
+          plot = $.jqplot(chartID, data.data, {
+            title: data.title,
+            height: 450,
+            width: 220,
+            stackSeries: true,
+            legend:{show: true},
+            grid:{background:'#fefbf3', borderWidth:2.5},
+            seriesDefaults: data.seriesDefaults,
+            axes: data.axes,
+            series: data.series
           });
-      }
+        }
       });
     }
   });
