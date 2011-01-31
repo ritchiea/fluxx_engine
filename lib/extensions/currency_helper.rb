547fa047927b4475ca5e858ee1e8e48301f35a8b
Eric Hansen <ehansen486@gmail.com>
2011-01-31 14:19:08 -0800

    add helper methods

--- lib/extensions/currency_helper.rb
+++ lib/extensions/currency_helper.rb
@@ -7,4 +7,12 @@ class CurrencyHelper
   def self.translate_symbol_to_name currency_symbol
     CURRENCY_MAPPER_FROM_SIGN[currency_symbol]
   end
+  def self.translate_symbol_to_long_name currency_symbol
+    currency = CURRENCY_MAPPER_FROM_SIGN[currency_symbol]
+    currency ? currency[:long_name] : nil
+  end
+  def self.translate_symbol_to_short_name currency_symbol
+    currency = CURRENCY_MAPPER_FROM_SIGN[currency_symbol]
+    currency ? currency[:short_name] : nil
+  end
 end
