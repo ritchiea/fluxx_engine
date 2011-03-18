415251ce7ccd9c662521020c87b5ab02f48a6702
Andrew Lee <drew@intramedia.net>
2011-03-18 11:13:30 -1000

    FLX-214 Display template layout if set when form validation fails

--- lib/extensions/action_controller/base.rb
+++ lib/extensions/action_controller/base.rb
@@ -358,7 +358,7 @@ class ActionController::Base
             p "ESH: error saving record #{@model.errors.inspect}"
             flash[:error] = t(:errors_were_found) unless flash[:error]
             logger.debug("Unable to create "+create_object.singular_model_instance_name.to_s+" with errors="+@model.errors.inspect)
-            format.html { render((create_object.view || "#{insta_path}/new").to_s, :layout => false) }
+            format.html { render((create_object.view || "#{insta_path}/new").to_s, :layout => create_object.layout) }
             # TODO ESH: revisit what to send back for JSON error
             format.json { head 500 }
             format.xml  { render :xml => @model.errors, :status => :unprocessable_entity }
@@ -440,7 +440,7 @@ class ActionController::Base
             flash[:error] = t(:errors_were_found) unless flash[:error]
             insta_respond_to update_object, :error do |format|
               logger.debug("Unable to save "+update_object.singular_model_instance_name.to_s+" with errors="+@model.errors.inspect)
-              format.html { render((update_object.view || "#{insta_path}/edit").to_s, :layout => false) }
+              format.html { render((update_object.view || "#{insta_path}/edit").to_s, :layout => update_object.layout) }
               # TODO ESH: revisit what to send back for JSON error
               format.json { head 500 }
               format.xml  { render :xml => @model.errors, :status => :unprocessable_entity }
@@ -453,7 +453,7 @@ class ActionController::Base
           @not_editable=true
           insta_respond_to update_object, :locked do |format|
             # Provide a locked error message
-            format.html { render((update_object.view || "#{insta_path}/edit").to_s, :layout => false) }
+            format.html { render((update_object.view || "#{insta_path}/edit").to_s, :layout => update_object.layout) }
             # TODO ESH: revisit what to send back for JSON error
             format.json { head 500 }
             format.xml  { render :xml => @model.errors, :status => :unprocessable_entity }
