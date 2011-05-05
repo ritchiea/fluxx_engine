dfaade6bdcb84b40988aec40abf5e049a226c06b
Eric Hansen <ehansen486@gmail.com>
2011-05-04 17:12:52 -0700

    rework localization files to simplify things dramatically

--- lib/fluxx_engine/engine.rb
+++ lib/fluxx_engine/engine.rb
@@ -3,6 +3,7 @@ require "action_controller"
 
 module FluxxEngine
   class Engine < Rails::Engine
+    config.i18n.load_path += Dir["#{File.dirname(__FILE__).to_s}/../../config/fluxx_locales/*.{rb,yml}"]
     initializer 'fluxx_engine.add_compass_hooks', :after=> :disable_dependency_loading do |app|
       Sass::Plugin.add_template_location "#{File.dirname(__FILE__).to_s}/../../app/stylesheets", "public/stylesheets/compiled/fluxx_engine"
     end
