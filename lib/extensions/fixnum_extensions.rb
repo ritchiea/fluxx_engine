9f93c2586e1006f5f0ea78c4c9b33f3652f604e3
Eric Hansen <ehansen486@gmail.com>
2011-05-31 11:57:38 -0700

    add in bigdecimal to_currency

--- lib/extensions/fixnum_extensions.rb
+++ lib/extensions/fixnum_extensions.rb
@@ -1,6 +1,16 @@
 require 'rubygems'
 require 'action_view'
 
+class BigDecimal
+  def to_currency(options = {})
+    ActionView::Base.new.number_to_currency(self, options)
+  end
+  def to_currency_no_cents(options = {})
+    options[:precision] = options[:precision] || 0
+    ActionView::Base.new.number_to_currency(self, options)
+  end
+end
+
 class Fixnum
   def to_currency(options = {})
     ActionView::Base.new.number_to_currency(self, options)
