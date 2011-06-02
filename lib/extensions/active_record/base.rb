fec26a0b8109a241086d27bd96858e40cdc2c355
Eric Hansen <ehansen486@gmail.com>
2011-06-02 09:44:56 -0700

    fix issue with 3.0.7

--- lib/extensions/active_record/base.rb
+++ lib/extensions/active_record/base.rb
@@ -251,7 +251,7 @@ class ActiveRecord::Base
     if model_ids.nil? || model_ids.empty?
       model_ids
     else
-      unpaged_models = self.find model_ids
+      unpaged_models = self.where(:id => model_ids).all
       unpaged_models = [unpaged_models] unless unpaged_models.is_a?(Array)
       model_map = unpaged_models.inject({}) {|acc, model| acc[model.id] = model; acc}
       ordered_list = model_ids.map {|model_id| model_map[model_id]}
