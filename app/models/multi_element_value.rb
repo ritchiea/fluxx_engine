0a9f3ca9d565fb42ce0fee07e08a3e13d5b3d323
Eric Hansen <ehansen486@gmail.com>
2011-06-16 21:12:03 -0700

    multi element value allow search by multi element group

--- app/models/multi_element_value.rb
+++ app/models/multi_element_value.rb
@@ -3,7 +3,7 @@ class MultiElementValue < ActiveRecord::Base
   belongs_to :multi_element_group
   belongs_to :dependent_multi_element_value, :class_name => 'MultiElementValue', :foreign_key => 'dependent_multi_element_value_id'
 
-  SEARCH_ATTRIBUTES = [:dependent_multi_element_value_id]
+  SEARCH_ATTRIBUTES = [:dependent_multi_element_value_id, :multi_element_group_id]
   insta_search do |insta|
     insta.filter_fields = SEARCH_ATTRIBUTES
   end
