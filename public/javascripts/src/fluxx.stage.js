555689f31435c41dd9205b08a1562d689de1086c
Andrew Lee <drew@intramedia.net>
2012-02-17 15:32:42 -1000

    FLX-2091 New UI Component - Dynamic Form Field Table

--- public/javascripts/src/fluxx.stage.js
+++ public/javascripts/src/fluxx.stage.js
@@ -1583,11 +1583,16 @@
             'change', function(e) {
               e.preventDefault();
               var $elem = $(this);
+              //TODO AML: {symbol: "$"}  - set the currency symbol
+              if ($elem.parent().hasClass('amount'))
+                $elem.formatCurrency();
+              else
+                $elem.toNumber();
               var $card = $elem.fluxxCard();
               var total = 0;
               $('[data-result-field="' + $elem.attr('data-result-field') + '"]', $card).not($($elem.attr('data-result-field'))).each(function() {
                 var $field = $(this);
-                var float = parseFloat($.fluxx.getVal($field));
+                var float = $field.asNumber()
                 if ($field.hasClass('sum') && !isNaN(float)) {
                   total = total + float;
                 }
