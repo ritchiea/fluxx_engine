f7e9aa15e1faefc79d546a10da44c3e8c5df79e7
Andrew Lee <drew@intramedia.net>
2012-03-09 19:47:52 -1000

    FLX-2094 table row formatter to help in liquid docs

--- lib/extensions/liquid_extension.rb
+++ lib/extensions/liquid_extension.rb
@@ -244,14 +244,16 @@ module LiquidFilters
   end
 
   def format_table_row values, row, num_cols
-    row = row.to_i
-    col = "a"
-    cols = []
-    for i in 1..num_cols.to_i
-      cols.push values["#{col}#{row}"]
-      col.succ!
+    if values
+      row = row.to_i
+      col = "a"
+      cols = []
+      for i in 1..num_cols.to_i
+        cols.push values["#{col}#{row}"]
+        col.succ!
+      end
+      "<td>#{cols.join('</td><td>')}</td>" if cols[0]
     end
-    "<td>#{cols.join('</td><td>')}</td>" if cols[0]
   end
 end
 
